cae66cf3bec8240a8079b3d9053416b8a97e6682
Daniil Nikolenko <qoo2p5@gmail.com>
2021-08-16 01:12:21 +0300

    Add comments

--- bot/state.py
+++ bot/state.py
@@ -2,22 +2,29 @@ from enum import Enum
 from typing import Dict, List
 
 
+# Предполагается, что объект игры Game будет создаваться при вызове команды /create
+# и будет присваиваться полю creating_game пользователя, который её создал.
+# Далее, в процессе создания пользователем игры будет заполняться поле role_count.
+# После того, как пользователь закончил создание игры, игре должен быть присвоен уникальный game_id,
+# и после этого игра должна быть добавлена в game_by_id у State.
+# Когда нужное количество пользователей присоединится к игре, нужно случайно распределить роли,
+# отправить их игрокам, сбросить их статусы, а также удалить игру из game_by_id.
+
+
 class Game:
     role_count: Dict[str, int]
-    started: bool
     joined_user_ids: List[str]
     role_by_user_id: Dict[str, str]  # заполняется, когда игра начинается, то есть все игроки присоединились
 
     def __init__(self):
         self.role_count = dict()
-        self.started = False
         self.joined_user_ids = list()
         self.role_by_user_id = dict()
 
 
 class UserStatus(Enum):
     NONE = 0
-    IN_GAME = 1
+    JOINED = 1
     CREATING = 2
     JOINING = 3
 
